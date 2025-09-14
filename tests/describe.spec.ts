f2147a4dbc86b769ee88b86b541b04f5d530fde4
Naeem Malik <naeem.akram.malik@gmail.com>
2025-09-14 20:48:27 +0500

    Added an extra test in describe to show usage with -g and file name

--- tests/describe.spec.ts
+++ tests/describe.spec.ts
@@ -5,6 +5,7 @@ test.describe('Login Feature', () => {
     test.beforeEach(async ({ page }) => {
     await page.goto('/');
 });
+
     test('valid user should see dashboard', async ({ page }) => {
         await page.getByRole('textbox', { name: 'Enter your email address' }).fill('admin@testautomationtv.com');
         await page.getByRole('textbox', { name: 'Enter your password' }).fill('admin123');
@@ -18,5 +19,13 @@ test.describe('Login Feature', () => {
         await page.getByRole('button', { name: 'Sign in to your account' }).click();
         await expect(page.getByRole('alert')).toHaveText(/invalid/i);
     });
+
+});
+
+test.describe('QA Form Demo',  () => {
+    test('Is Guest Book Opening', async ({page}) => {
+        await page.goto('/guestbook');
+        await expect(page.getByRole('heading', { name: 'Guest Book page title' })).toBeVisible();        
+});
 
 });
