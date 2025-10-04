2818689ce4546ad3781862dc3dfebbe9b642b449
Naeem Malik <naeem.akram.malik@gmail.com>
2025-10-04 20:12:37 +0500

    Written test for file download

--- tests/file-uploads.spec.js
+++ tests/file-uploads.spec.js
@@ -9,3 +9,24 @@ test('should upload a file successfully', async ({page}) => {
     await expect(page.getByRole('alert')).toHaveText('File "test-file.txt" uploaded successfully!',
     { timeout: 10000 });
 });
+
+test('should handle file download', async ({ page }) => {
+  await page.goto('/file-demo');
+
+  const [download] = await Promise.all([
+    page.waitForEvent('download'),
+    page.getByRole('link', { name: /Download Text/ }).click()
+  ]);
+
+  // Wait until the file is fully downloaded
+  const path = await download.path();
+  const suggestedName = download.suggestedFilename();
+
+  console.log(`Downloaded file path: ${path}`);
+  console.log(`Suggested file name: ${suggestedName}`);
+
+  expect(suggestedName).toBe('sample.txt');
+
+  const fileSize = (await download.createReadStream())?.readableLength;
+  console.log(`File size: ${fileSize} bytes`);
+});
